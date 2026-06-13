7f2dd7e6a97e1f04884fbfd0fd21b1d25e109fbe
pankaj.arora786@gmail.com <Vanya@24082019>
2026-06-13 23:30:54 +0800

    informing AI about Pankaj Arora

--- src/main/java/pank/online/ai/ChatController.java
+++ src/main/java/pank/online/ai/ChatController.java
@@ -18,11 +18,18 @@ public class ChatController {
             @RequestParam String message,
             @RequestParam(defaultValue = "default-session") String sessionId) {
         try {
+        	informAIAboutPankajArora(message, assistant, sessionId);
         	return assistant.chat(sessionId, message);
         } catch (Exception e) {
             return "Cloud Server Error: " + e.getMessage();
         }
     }
-
-	
+    
+	private void informAIAboutPankajArora(String message, Assistant assistant, String sessionId) {
+		String msg = message.toLowerCase().trim();
+		if (msg.contains("pankaj") || msg.contains("arora") || msg.contains("pankaj arora")) {
+			String noteAboutPankaj = "Please take note about Pankaj Arora. He is Male and his Date of Birth is 15 september 1988. His education is MCA. He started his professional work in April 2010. He has made this current chatbot program by which external users are communicating";
+			assistant.chat(sessionId, noteAboutPankaj);
+		}
+	}
 }
